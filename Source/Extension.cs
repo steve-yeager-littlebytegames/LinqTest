4fab37f8ba14e202ca41e8227d1fcb0ca03919cb
Steve Yeager <steve.yeager@littlebytegames.com>
2021-02-23 17:56:36 -0800

    How about this?

---
+++ Source/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Source
+{
+    public static class Extension
+    {
+        public static KeyValuePair<T, U>? FirstOrNullStruct<T,U>(this IDictionary<T,U> list, U value)
+        {
+            var found = list.FirstOrDefault(kvp => kvp.Value.Equals(value));
+            if(found.Equals(default(KeyValuePair<T,U>)))
+            {
+                return null;
+            }
+		
+            return found;
+        }
+    }
+}
---
+++ Source/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Source
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new SortedList<int, int>();
+            list.Add(10, 1);
+            list.Add(30, 2);
+            list.Add(150, 3);
+
+            while(true)
+            {
+                var line = Console.ReadLine();
+                var input = int.Parse(line);
+                var found = list.FirstOrNullStruct(input);
+                if(found != null)
+                {
+                    Console.WriteLine($"Found {found.Value.Value} at {found.Value.Key}");
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found");
+                }
+            }
+        }
+    }
+}
